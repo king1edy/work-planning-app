85a9d7b96c9f2a26f3924ab04ca2250bb89f2bd6
Godswill David <davidgodswill@gmail.com>
2023-05-02 22:48:01 +0100

    added basics files structures

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), UsersModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
